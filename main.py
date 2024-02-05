d791b5668d4fc42f80137cebc461e60e8c2b257e
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-02-05 11:11:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import flet as mobil
 def main(page: mobil.Page) -> None:
     page.add(
-        mobil.Text(value='Hello user!', color='red')
+        mobil.Text(value='Rác Kevi Tamás vagyok.', color='blue'),
+        mobil.Text(value='A kedvenc filmem: Bright.', color='red'),
+        mobil.Text(value='Ahová szeretnék utazni: szinte bárhova, de elsőre tengerre.', color='yellow'),
     )
     page.update()
 if __name__== '__main__':
