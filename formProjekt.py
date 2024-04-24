cb8c80f38f43a372f27e56a8bc6994ed3a0542bb
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-04-24 09:25:45 +0200

    Add files via upload

--- formProjekt.py
+++ formProjekt.py
@@ -1,7 +1,29 @@
 import flet as ft
+
 def main(page: ft.Page) -> None:
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.theme_mode = ft.ThemeMode.DARK
+
     def button_clicked(e):
-        t1.value = f"Név: {tx1.value}, neme: {dd.value}, {radiogroup.value}-ban lakik és hobbija {c1.value}, {c2.value}, {c3.value}, {c4.value}."
+        name = tx1.value.strip()  
+        words = name.split()  
+
+        
+        if len(words) < 2:
+            t1.value = "Legalább két szóból kell állnia a névnek!"
+            page.update()
+            return
+
+        if not all(word.istitle() for word in words):
+            t1.value = "Minden szónak nagybetűvel kell kezdődnie!"
+            page.update()
+            return
+        
+
+        t1.value = f"Név: {tx1.value}, neme: {dd.value}, {radiogroup.value}-ban lakik és hobbija \n\tSport: {c1.value}, \n\tFőzés: {c2.value}, \n\tSzámítógépes játék: {c3.value}, \n\tÁllatok {c4.value}."
         page.update() 
 
         
@@ -14,12 +36,20 @@ def main(page: ft.Page) -> None:
         width=300
     )
 
+    t2 = ft.Text(
+        value="Mi a nemed?"
+    )
+
     dd = ft.Dropdown(
         width=300,
         options=[
             ft.dropdown.Option("Női"),
             ft.dropdown.Option("Férfi")
         ]
+    )
+
+    t3 = ft.Text(
+        value="Hol laksz?"
     )
 
     radiogroup = ft.RadioGroup(
@@ -31,11 +61,14 @@ def main(page: ft.Page) -> None:
         ])
     )
 
-    
-    c1 = ft.Checkbox(label="Sport", width=300, value="sport"),
-    c2 = ft.Checkbox(label="Főzés", width=300, value="főzés"),
-    c3 = ft.Checkbox(label="Számítógépes játék", width=300, value="számítógépes játék"),
-    c4 = ft.Checkbox(label="Állatok", width=300, value="állatok")
+    t4 = ft.Text(
+        value="Mi a hobbid?"
+    )
+
+    c1 = ft.Checkbox(label="Sport", value=False)
+    c2 = ft.Checkbox(label="Főzés", value=False)
+    c3 = ft.Checkbox(label="Számítógépes játék", value=False)
+    c4 = ft.Checkbox(label="Állatok", value=False)
     
 
     button = ft.ElevatedButton(
@@ -46,8 +79,9 @@ def main(page: ft.Page) -> None:
     form = ft.Container(
         border=ft.border.all(3, 'red'),
         padding=20,
+        width=600,
         alignment=ft.alignment.center,
-        content = ft.Column([tx1, dd, radiogroup, c1, c2, c3, c4, button])
+        content = ft.Column([tx1, t2, dd, t3, radiogroup, t4, c1, c2, c3, c4, button])
     )
 
     
