99d08af7b82b746b685e274aeb1e18dc956ffce2
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-06-03 13:04:32 +0200

    Add files via upload

--- chatgpt.py
+++ chatgpt.py
@@ -3,7 +3,9 @@ import flet as ft
 
 
 def main(page: ft.Page) -> None:
-    
+    page.theme_mode = ft.ThemeMode.DARK
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
 
 # Az API kulcsot itt kell megadni
     api_key = 'sk-proj-zO6LNh5AjjS93VRYnHlzT3BlbkFJCQanAg4huDsWatz718HH'
@@ -18,19 +20,24 @@ def main(page: ft.Page) -> None:
         )
         return response.choices[0].text.strip()
 
-def main(page: ft.Page) -> None:
-    ft.Text("Üdvözöllek a ChatGPT programban! Kilépéshez írd be: 'exit'")
+
+    page.add(ft.Text("Üdvözöllek a ChatGPT programban! Kilépéshez írd be: 'exit'"))
         
     while True:
         user_input = ft.TextField(value="Te: ")
-        if user_input == 'exit':
+        page.add(user_input)
+        if user_input.value == 'exit':
             ft.Text("Köszönöm, hogy használtad a ChatGPT programot!")
             break
             
-        response = chat(user_input)
-        ft.Text(f"ChatGPT: {response}")
-        
-    page.add()
+        page.add(response = chat(user_input))
+        page.update()
+        # print(response)
+        page.add(
+            ft.Text(f"ChatGPT: {response}")
+        )
+        page.update()
+
 
 if __name__ == "__main__":
     ft.app(target=main)
